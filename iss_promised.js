f196c9a2900a2e3e957a39a19ce99239f002bdc8
Kelsey Erickson <kerickson@gmx.com>
2021-08-21 16:22:05 +0000

    Fix lint errors

--- iss_promised.js
+++ iss_promised.js
@@ -3,7 +3,7 @@ const request = require('request-promise-native');
 const fetchMyIP = () => {
 
 
-  return request('https://api.ipify.org?format=json')
+  return request('https://api.ipify.org?format=json');
 
 
 };
@@ -11,30 +11,30 @@ const fetchMyIP = () => {
 const fetchCoordsByIP = (body) => {
 
   const ip = JSON.parse(body).ip;
-  return request(`https://freegeoip.app/json/${ip}`)
+  return request(`https://freegeoip.app/json/${ip}`);
 
 };
 
 const fetchISSFlyOverTimes = (body) => {
 
-  const {latitude, longitude} = JSON.parse(body);
-  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`)
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
 
 
 };
 
 const nextISSTimesForMyLocation = () => {
- 
+
   return fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then((data) => {
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
 
-    const response  = JSON.parse(data).response;
-    return response;
+      const response = JSON.parse(data).response;
+      return response;
 
 
-  });
+    });
 
 
 };
